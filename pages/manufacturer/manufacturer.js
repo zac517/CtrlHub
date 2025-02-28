6da04024d94dc61e9607b20db17321879fb470d3
zac517 <1271470655@qq.com>
2025-02-28 15:39:04 +0800

    添加产品选择界面

--- pages/manufacturer/manufacturer.js
+++ pages/manufacturer/manufacturer.js
@@ -1,18 +1,19 @@
 // pages/manufacturer/manufacturer.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+  },
 
+  // 返回函数
+  backToHome() {
+    wx.redirectTo({
+      url: '/pages/home/home',
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad() {
   },
 
   /**
